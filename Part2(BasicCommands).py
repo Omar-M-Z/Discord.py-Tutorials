55f2a0b3bb39e152a48b935c6895175a95c7ae78
Omar-M-Z <67700034+Omar-M-Z@users.noreply.github.com>
2021-02-20 13:44:16 +0300

    File updated

--- Part2(BasicCommands).py
+++ Part2(BasicCommands).py
@@ -13,9 +13,31 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hi!")
 
-#Command that makes bot say "Hi!"
+#Command that makes bot send information about the context of the command
 @client.command()
-async def printinformation(ctx):
+async def sendinformation(ctx):
+    #sends name of server the command is sent in
+    await ctx.send(ctx.guild)
+    #sends name of the user that sends the command
+    await ctx.send(ctx.author)
+    #sends name of the channel in which the command was sent
+    await ctx.send(ctx.channel)
+    
+#Command that takes two arguments from user and sends them
+@client.command()
+async def sendarguments(ctx, arg1, arg2):
+    await ctx.send(arg1)
+    await ctx.send(arg2)
 
+#Command that with name that is different from the name of the function and has aliases
+@client.command(name = "helloworld", aliases = ["hiworld", "greetingsworld"])
+async def examplecommand(ctx):
+    await ctx.send("Hello World")
 
-client.run("ODA4MzExNzc1MjA0MzQzODM4.YCEtAA.hQnYndW7xlyXbSCW1bpyCdA7uJI")
+#Command that checks if user has permission to kick members from a server
+@client.command()
+@commands.has_permissions(kick_members = True)
+async def kick(ctx):
+    await ctx.send("You can kick members and abuse your permissions!")
+
+client.run(<Bot Token Goes Here>)
